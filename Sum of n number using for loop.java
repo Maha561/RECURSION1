577817a72c8a695d51a7c75330ef7e8cc7864965
Maha561 <mahalakshmianandan04@gmail.com>
2026-01-11 15:16:49 +0530

    added

---
+++ Sum of n number using for loop.java
@@ -0,0 +1,20 @@
+
+
+// sum of n numbers
+// for example lets say n = 3 , so 1+2+3 =6 which is called sum of n numbers.
+// using normal for loop
+public class Main {
+    public static void main(String[] args) {
+
+        int sum = 0;
+        int n = 3;
+        for(int i =1;i<=n;i++){
+
+            sum = sum+i;
+        }
+
+        System.out.println(sum);
+
+
+    }
+}
