f3207b7f43d2d263c72e11b7a28121a4526965e7
Maha561 <mahalakshmianandan04@gmail.com>
2026-01-11 15:22:24 +0530

    added

---
+++ SumOfN_usingForloop.java
@@ -0,0 +1,17 @@
+public class SumOfN_usingForloop {
+
+     public static void main(String[] args) {
+
+        int sum = 0;
+        int n = 3;
+        for(int i =1;i<=n;i++){
+
+            sum = sum+i;
+        }
+
+        System.out.println(sum);
+
+
+    }
+    
+}
